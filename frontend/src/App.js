d05d2081d8dd2e86d51d3156bc29d68ae945c68b
Pabitra Kumar Mohanty <77189083+pkmofficial@users.noreply.github.com>
2023-07-04 16:36:34 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ import  {Toaster} from "react-hot-toast"
 
 function App() {
 
-    const BASE_URL = "https://auth-todo-list-mern-production-e11d.up.railway.app";
+    const BASE_URL = "https://mern-crud-server-seven.vercel.app";
       
 
   return (
