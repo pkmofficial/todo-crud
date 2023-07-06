44c13df39449787e9fa5e8f724c083cb78246e4e
pkmofficial <pabitrakumarm084@gmail.com>
2023-07-06 11:29:26 +0530

    styled login and register page using Tailwind CSS

--- frontend/src/components/registration/Login.js
+++ frontend/src/components/registration/Login.js
@@ -44,9 +44,9 @@ function Login({BASE_URL}) {
   return (
     <>
       {/* new code */}
-      <section class="bg-gray-50 dark:bg-gray-900">
+      {/* <section class="bg-gray-50 ">
         <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-            <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+            <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-white-900 ">
                 <img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo" />
                 Todo Application    
             </a>
@@ -55,7 +55,7 @@ function Login({BASE_URL}) {
                     <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                         Sign in to your account
                     </h1>
-                    <form class="space-y-4 md:space-y-6" onSubmit={submitLogin} className="space-y-6" action="#" method="POST">
+                    <form  onSubmit={submitLogin} className="space-y-6" action="#" method="POST">
                         <div>
                             <label htmlFor="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
                             <input id="email"
@@ -86,7 +86,90 @@ function Login({BASE_URL}) {
                 </div>
             </div>
         </div>
-      </section>
+      </section> */}
+
+      {/* new */}
+      <h1 className="mt-7 text-center text-2xl font-extrabold leading-9 tracking-tight text-blue-900 ">
+            Todo CRUD Application
+      </h1>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+
+          {/* <img
+            className="mx-auto h-10 w-auto"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            alt="Your Company"
+          /> */}
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-blue-900">
+            Sign in to your account
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" onSubmit={submitLogin} action="#" method="POST">
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-blue-700">
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  value={email}
+                  onChange={(e)=>{setEmail(e.target.value)}}
+                  placeholder='sample@test.com'
+                  required
+                  className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-blue-700">
+                  Password
+                </label>
+                {/* <div className="text-sm">
+                  <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                    Already have an account?
+                  </a>
+                </div> */}
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  value={password}
+                  onChange={(e)=>{setPassword(e.target.value)}}
+                  placeholder='enter password'
+                  required
+                  className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm text-gray-500">
+            Don't have an account?{' '}
+            <a href="/signup" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+              Register Now.
+            </a>
+          </p>
+        </div>
+      </div>
       
     </>
   )
--- frontend/src/components/registration/Register.js
+++ frontend/src/components/registration/Register.js
@@ -54,7 +54,7 @@ function Register({BASE_URL}) {
 
   return (
     <>
-    <div className="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+    {/* <div className="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
     <div className="text-center text-2xl font-bold">Sign up</div>
     <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
       <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
@@ -72,8 +72,8 @@ function Register({BASE_URL}) {
                 name="firstName"
                 type="text"
                 autoComplete="firstName"
-                  value={userDetails.firstName}
-                  onChange ={handleDetails}
+                value={userDetails.firstName}
+                onChange ={handleDetails}
                 required
                 className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
               />
@@ -163,7 +163,123 @@ function Register({BASE_URL}) {
         </div>
       </div>
     </div>
-  </div>
+  </div> */}
+
+  {/* NEW */}
+  <h1 className="mt-7 text-center text-2xl font-extrabold leading-9 tracking-tight text-blue-900 ">
+            Todo CRUD Application
+      </h1>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-8 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+
+          {/* <img
+            className="mx-auto h-10 w-auto"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            alt="Your Company"
+          /> */}
+          <h2 className="mt-5 text-center text-2xl font-bold leading-9 tracking-tight text-blue-900">
+          Create an accoount
+          </h2>
+        </div>
+
+        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" onSubmit={handleSubmit} action="#" method="POST">
+            <div>
+              <label htmlFor="firstName" className="block text-sm font-medium leading-6 text-blue-700">
+                First Name
+              </label>
+              <div className="mt-2">
+                <input
+                  id="firstName"
+                  name="firstName"
+                  type="text"
+                  autoComplete="firstName"
+                  value={userDetails.firstName}
+                  onChange={handleDetails}
+                  required
+                  className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label htmlFor="firstName" className="block text-sm font-medium leading-6 text-blue-700">
+                Last Name
+              </label>
+              <div className="mt-2">
+                <input
+                  id="lastName"
+                  name="lastName"
+                  type="text"
+                  autoComplete="lastName"
+                  value={userDetails.lastName}
+                  onChange={handleDetails}
+                  required
+                  className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-blue-700">
+              Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  required
+                  value={userDetails.email}
+                  onChange ={handleDetails}
+                  className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-blue-700">
+                  Password
+                </label>
+                {/* <div className="text-sm">
+                  <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                    Already have an account?
+                  </a>
+                </div> */}
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  value={userDetails.password}
+                  onChange ={handleDetails}
+                  required
+                  className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Register
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-5 text-center text-sm text-gray-500">
+            Already have an account??{' '}
+            <NavLink to={"/"} className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+                  Sign In
+            </NavLink >
+          </p>
+        </div>
+      </div>
 </>
   )
 }
